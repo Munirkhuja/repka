00373fcaed1979d273009597b5a963ed11744933
munirhuja1357@gmail.com <135711_git>
2023-03-03 17:56:38 +0500

    first commit

---
+++ resources/lang/en/theme_directory_hub.php
@@ -0,0 +1,159 @@
+<?php
+
+return array (
+  'alert' => 
+  array (
+    'theme-activated' => 'The theme has activated',
+    'theme-color-restore-confirm' => 'Are you sure you want to restore to theme default colors?',
+    'theme-color-restored' => 'The theme colors have been restored to default values',
+    'theme-color-updated' => 'The theme colors have been updated',
+    'theme-debug' => 'Cannot delete theme in THEME_DEBUG mode.',
+    'theme-delete-confirm' => 'Are you sure you want to delete this theme?',
+    'theme-delete-default-theme' => 'Cannot delete the system default theme',
+    'theme-deleted' => 'Theme has been deleted',
+    'theme-header-restore-confirm' => 'Are you sure you want to restore theme headers to default values?',
+    'theme-header-restored' => 'The theme headers have been restored to default values',
+    'theme-header-updated' => 'The theme headers have been updated',
+    'theme-install-failed-open-package' => 'Failed to open theme install zip package',
+    'theme-installed' => 'New theme installed successfully',
+  ),
+  'email' => 
+  array (
+    'alert' => 
+    array (
+      'sending-problem' => 'We are having a problem sending out emails. If the problem persists, please contact the website administrator.',
+    ),
+  ),
+  'filter-button-filter-results' => 'Filter Results',
+  'filter-filter-by' => 'Filter by',
+  'filter-link-reset-all' => 'Reset',
+  'filter-results' => 'Results',
+  'filter-sort-by-nearby-first' => 'Nearby First',
+  'importer' => 
+  array (
+    'basic' => 'Basic',
+    'custom-field' => 'Custom Field',
+    'import-custom-field-help' => 'The importable custom fields depending on the category you select while importing. So you may not able to import all custom fields and values presented in this form.',
+    'import-error' => 
+    array (
+      'item-type-not-exist' => 'A listing must in regular listing type or online listing type',
+    ),
+    'random-user' => 'Pick up a random user',
+  ),
+  'listing' => 
+  array (
+    'gallery-upload-help' => 'maximum file size: 5mb per image, maximum :gallery_photos_count images, if more than :gallery_photos_count images selected, first :gallery_photos_count images will be saved.',
+    'qr-code' => 'QR Code',
+  ),
+  'online-listing' => 
+  array (
+    'listing-type' => 'Listing Type',
+    'online-listing' => 'On-demand service',
+    'online-listing-help' => 'For business with no physical address',
+    'regular-listing' => 'Regular Listing',
+    'regular-listing-help' => 'For business that has a physical address',
+  ),
+  'plan' => 
+  array (
+    'alert' => 
+    array (
+      'cannot-delete-free-plan' => 'Cannot delete system default free plan',
+      'free-plan-quota-reached' => 'You have reached max free business listing quota.',
+      'plan-period' => 'Plan period must in monthly, quarterly, or yearly.',
+    ),
+    'edit-plan-warning' => 'Changes to this plan will apply to all users\' subscriptions that in this plan. Please edit with caution.',
+    'featured-listing' => 'featured listing',
+    'free-listing' => 'free listing',
+    'free-listing-cap' => 'Free listing',
+    'free-plan' => 'Free Plan',
+    'max-featured-listing-help' => 'Leave it empty for an unlimited featured listing, enter 0 to prohibit featured listing.',
+    'max-free-listing' => 'Maximum Free Listing',
+    'max-free-listing-help' => 'Leave it empty for an unlimited free listing, enter 0 to prohibit free listing.',
+    'paid-plan' => 'Paid Plan',
+    'subscription-edn-date-help' => 'For free plan, the subscription end date will automatically set to null',
+    'unlimited' => 'Unlimited',
+  ),
+  'pricing' => 
+  array (
+    'active' => 'Active',
+    'footer' => 
+    array (
+      'pricing' => 'Pricing',
+    ),
+    'get-started' => 'Get Started',
+    'head-description' => 'Choose a plan that\'s right for your business',
+    'head-title' => 'Pricing',
+    'info-admin' => 'You are viewing pricing tables as Administrator, you can manage website pricing plans by click the Manage Pricing button in each table below.',
+    'info-user-free' => 'You are currently in a free subscription, consider upgrading to a paid subscription to unlock extra features brought by :site_name!',
+    'info-user-paid' => 'Woohoo! You had a paid subscription. Enjoy extra features brought by :site_name!',
+    'manage-pricing' => 'Manage Pricing',
+    'seo' => 
+    array (
+      'pricing' => 'Pricing - :site_name',
+    ),
+    'upgrade' => 'Upgrade',
+  ),
+  'seo' => 
+  array (
+    'color-edit' => 'Dashboard - Edit Theme Color - :site_name',
+    'header-edit' => 'Dashboard - Edit Theme Header - :site_name',
+    'index' => 'Dashboard - Themes - :site_name',
+    'install-theme' => 'Dashboard - Install Theme - :site_name',
+  ),
+  'setting' => 
+  array (
+    'alert' => 
+    array (
+      'setting-item-updated' => 'Listing setting updated successfully',
+    ),
+    'default-country' => 'Default Country',
+    'default-country-help' => 'The default country to load listing data from on frontend website',
+    'default-language' => 'Default Language',
+    'default-language-help' => 'Default website language',
+    'default-latitude' => 'Default Latitude',
+    'default-latitude-help' => 'Default latitude for map selector',
+    'default-longitude' => 'Default Longitude',
+    'default-longitude-help' => 'Default longitude for map selector',
+    'display-currency-symbol' => 'Display Currency Symbol',
+    'display-currency-symbol-help' => 'The currency symbol to display for prices wherever applicable on website',
+    'item-setting' => 'Listing Setting',
+    'item-setting-auto-approval' => 'Auto Approve',
+    'item-setting-auto-approval-help' => 'Auto approve listing submission for users',
+    'item-setting-desc' => 'This page allows you edit the configuration of listing.',
+    'item-setting-max-gallery-photos' => 'Maximum Gallery Photos',
+    'item-setting-max-gallery-photos-help' => 'The total gallery photos allowed to upload (min:1 max:20)',
+    'seo' => 
+    array (
+      'edit-item' => 'Dashboard - Listing Setting - :site_name',
+    ),
+    'warning-smtp' => 'Website SMTP information has not configured and enabled. Set up now to avoid system email delivery issues.',
+    'warning-smtp-action' => 'SMTP Setting',
+  ),
+  'sidebar' => 
+  array (
+    'install-theme' => 'Install Theme',
+    'manage-themes' => 'Manage Themes',
+    'themes' => 'Themes',
+  ),
+  'theme-active' => 'Active',
+  'theme-by-author' => 'By',
+  'theme-create' => 'Install/Update Theme',
+  'theme-create-desc' => 'This page allows you to install a new theme or update an existing theme to the website.',
+  'theme-current' => 'Current Theme',
+  'theme-description-directory-hub' => 'The Directory Hub Listing & Business Directory CMS default theme. Please use the Edit Colors and Edit Headers buttons to customize the theme styles of yours.',
+  'theme-detail' => 'Theme Detail',
+  'theme-edit-color' => 'Edit Theme Colors',
+  'theme-edit-color-desc' => 'This page allows you edit the theme specific colors. You can also restore to default colors.',
+  'theme-edit-colors' => 'Edit Colors',
+  'theme-edit-header' => 'Edit Theme Headers',
+  'theme-edit-header-desc' => 'This page allows you edit the theme specific headers. You can also restore to default values.',
+  'theme-edit-headers' => 'Edit Headers',
+  'theme-index' => 'Manage Themes',
+  'theme-index-desc' => 'This page allows you manage website themes. You can install, customize, active, inactive, or delete themes in this page.',
+  'theme-install' => 'Install/Update Theme',
+  'theme-install-button' => 'Submit',
+  'theme-install-label' => 'Select theme install/update package',
+  'theme-install-label-help' => 'The theme install/update package should have filename called theme_package.zip',
+  'theme-modal-close' => 'Close',
+  'theme-system-default' => 'Default',
+);
---
+++ storage/framework/views/a6239d28b9d00b48449907b7cd5c7cca5b7ed2fe.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>: <?php echo e($url); ?>
+
+<?php /**PATH /home2/fluffyc0/public_html/laravel_project/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
