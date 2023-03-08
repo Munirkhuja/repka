0d5004d53a69c7f157114aa7c292c84b7967f4ca
munirhuja1357@gmail.com <135711_git>
2023-03-09 01:08:12 +0500

    fix folder

--- laravel_project/app/Http/Controllers/HomeController.php
+++ laravel_project/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Setting;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        dd(Setting::all());
         return view('home');
     }
 }
